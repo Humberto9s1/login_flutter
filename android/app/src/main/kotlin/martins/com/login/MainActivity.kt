575020f674816738d5a5b31c0847456f3fe0b3e7
HUMBERTO MARTINS <humbertosant.mart@gmail.com>
2021-05-05 11:43:19 -0300

    inicio do projeto

---
+++ android/app/src/main/kotlin/martins/com/login/MainActivity.kt
@@ -0,0 +1,6 @@
+package martins.com.login
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class myHomePage extends StatefulWidget {
+  @override
+  _myHomePageState createState() => _myHomePageState();
+}
+
+class _myHomePageState extends State<myHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
